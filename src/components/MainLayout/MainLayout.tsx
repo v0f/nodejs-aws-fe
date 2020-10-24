9b135148f44549a5057edc6e11865b74fe065c63
vof <vof1111@gmail.com>
2020-10-24 23:53:02 +0300

    feat: aws task2 s3 + cloudfront

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -10,7 +10,7 @@ function Copyright() {
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
       <Link color="inherit" href="https://material-ui.com/">
-        My Store
+        My Store v1
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
