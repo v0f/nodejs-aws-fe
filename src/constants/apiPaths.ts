5738b1d8d2af3b6cbef893861c118027464959e6
vof <vof1111@gmail.com>
2020-11-16 18:31:56 +0300

    feat: update api path to import service

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://656aqwnv3i.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://656aqwnv3i.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://656aqwnv3i.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://odlak7nxvk.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://656aqwnv3i.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
